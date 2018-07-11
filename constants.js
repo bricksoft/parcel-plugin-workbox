b1f851c15206b3c80bd4b3b244bc796db0701251
Bricksoft <support@jabuco-it.de>
2018-07-11 17:17:04 +0200

    fix object spread => object.assign

--- constants.js
+++ constants.js
@@ -4,10 +4,11 @@ const defaultConfig = outDir => ({
   globPatterns: ["**/*.{js,css,html,png}"],
   swDest: path.resolve(outDir, "service-worker.js")
 });
-const workboxConfig = (outDir, pkg) => ({
-  ...defaultConfig(outDir),
-  ...(pkg[workboxConfigKey] || {})
-});
+const workboxConfig = (outDir, pkg) =>
+  Object.assign.apply(null, [
+    defaultConfig(outDir),
+    pkg[workboxConfigKey] || {}
+  ]);
 
 const INJECT = "injectManifest";
 const GENERATE = "generateSWString";
