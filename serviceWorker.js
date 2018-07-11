3042eb7adce1cf57c6c6f54333110c3044dd4b31
Bricksoft <support@jabuco-it.de>
2018-07-11 17:29:29 +0200

    fixed workbox implementation

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,69 @@
 const workboxBuild = require("workbox-build");
 const uglifyJS = require("uglify-js");
 const logger = require("parcel-bundler/src/Logger");
-const { writeFileSync } = require("fs");
-const { workboxTask, useMinifyDefault } = require("./constants");
+const { readFile, writeFileSync } = require("fs");
+const {
+  workboxTask,
+  useMinifyDefault,
+  defaultEncoding,
+  chars
+} = require("./constants");
+const formatBytes = require("./formatBytes.js");
 
 const getWorker = (options, minify = useMinifyDefault) => {
   const task = workboxTask(options);
+  let swData = {
+    count: 0,
+    size: 0,
+    warnings: []
+  };
 
   return workboxBuild[task](options)
-    .then(swString => {
-      logger.status("✓", "Service worker generated");
-      if (minify) {
-        swString = uglifyJS.minify(swString).code;
-        logger.status("✓", "Service worker minified");
+    .then(data => {
+      if (typeof data === "object") {
+        swData = data;
       }
+    })
+    .then(
+      src =>
+        typeof src !== "object"
+          ? `${src}`
+          : Promise.resolve(
+              new Promise((resolve, reject) =>
+                readFile(
+                  options.swDest,
+                  defaultEncoding,
+                  (err, data) => (err ? reject(err) : resolve(data))
+                )
+              )
+            )
+    )
+    .then(swString => {
+      logger.status(chars.success, "Service worker generated");
+      return swString;
+    })
+    .then(swString => {
+      let minified = swString;
+      if (minify) {
+        minified = uglifyJS.minify(minified).code;
+        logger.status(chars.success, "Service worker minified");
+      }
+      return minified;
+    })
+    .then(swString => {
       writeFileSync(options.swDest, swString);
-      logger.status("✓", "Service worker written : " + options.wDest);
+      logger.status(chars.success, `Workbox finished : ${options.swDest}`);
+    })
+    .then(() => {
+      let { size } = swData;
+      size = formatBytes(size);
+      logger.status(
+        chars.link,
+        `${swData.count} files will be precached, totalling ${size}.`
+      );
     })
     .catch(err => {
-      logger.error(err);
+      logger.status(chars.error, `Workbox failed : ${err}`);
     });
 };
 
