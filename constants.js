07bb8b3c5ef99d255b6abba1df27a2550ef1de07
Bricksoft <support@jabuco-it.de>
2018-07-11 17:15:57 +0200

    fix resolve path

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ const workboxConfigKey = "workbox";
 const defaultConfig = outDir => ({
   globDirectory: outDir,
   globPatterns: ["**/*.{js,css,html,png}"],
-  swDest: path.resolve(outDir, "/service-worker.js")
+  swDest: path.resolve(outDir, "service-worker.js")
 });
 const workboxConfig = (outDir, pkg) => ({
   ...defaultConfig(outDir),
