7d01f115dbc1539b882f4fcc8b7d077b7eecb574
Bricksoft <support@jabuco-it.de>
2018-07-11 17:27:53 +0200

    add byte formatter for human readable output

---
+++ formatBytes.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+// https://stackoverflow.com/a/18650828/3793309
+function formatBytes(a, b) {
+  if (0 == a) return "0 Bytes";
+  var c = 1024,
+    d = b || 2,
+    e = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"],
+    f = Math.floor(Math.log(a) / Math.log(c));
+  return parseFloat((a / Math.pow(c, f)).toFixed(d)) + " " + e[f];
+}
+
+module.exports = formatBytes;
